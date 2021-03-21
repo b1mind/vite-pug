ba7af6bc6dbd47bf6eda69af298512c43c7579fb
b1mind <brent@1mindsolutions.com>
2021-03-21 04:30:48 -0600

    🐛 nav needs proper routes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import pugPlugin from 'vite-plugin-pug'
 
+const { resolve } = require('path')
+
 export default defineConfig({
   plugins: [pugPlugin()],
+
   server: { port: 9001 },
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        page: resolve(__dirname, 'pages/page.html'),
+        page2: resolve(__dirname, 'pages/page2.html'),
+      },
+    },
+  },
 })
