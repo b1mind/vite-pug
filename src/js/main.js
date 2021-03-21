239a5f6e3de2ba3cf31ac8c876fc6c0ca65722a2
b1mind <brent@1mindsolutions.com>
2021-03-21 00:19:11 -0600

    🧹 Restructured files 📃 dev muli-pages model ✅ added todo / readme

---
+++ src/js/main.js
@@ -0,0 +1,1 @@
+console.log('I am Hello Pug World')
--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import pugPlugin from 'vite-plugin-pug'
 
 export default defineConfig({
   plugins: [pugPlugin()],
+  server: { port: 9001 },
 })
